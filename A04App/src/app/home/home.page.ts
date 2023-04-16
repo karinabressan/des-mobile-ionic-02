7f15f21ffd44fb451c128052f8ca8447519601dc
Karina <karina.felicetti@ngi.com.br>
2023-04-15 22:44:12 -0300

    removendo método não utilizado

--- A04App/src/app/home/home.page.ts
+++ A04App/src/app/home/home.page.ts
@@ -22,18 +22,5 @@ export class HomePage {
   removeFromList(indice:number){
     this.itens.splice(indice,1);
   }
-
-  aa(){
-
-    this.itens.push("item 1");
-    this.itens.push("item 1");
-
-    for(let i = 0; i < this.itens.length;i++){
-      let item = this.itens[i];
-      console.log(item);
-
-    }
-    
-  }
   
 }
