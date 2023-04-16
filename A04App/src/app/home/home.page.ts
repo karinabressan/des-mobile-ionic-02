a0f5053f66d731608f0c43840f0403c11ed82d1f
Karina <karina.felicetti@ngi.com.br>
2023-04-15 22:42:41 -0300

    Alterando a funcionalidade do botão Adicionar para usar formulário.Removendo os itens da lista a partir do botão excluir.

--- A04App/src/app/home/home.page.ts
+++ A04App/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -7,16 +8,32 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
-
+  novoProduto:string="";
   itens:string[] = [];
   constructor() {}
 
-  addToList(item:any){
+  addToList(item:string){
     this.itens.push(item);
   }
 
+  removeFromList(indice:number){
+    this.itens.splice(indice,1);
+  }
+
+  aa(){
+
+    this.itens.push("item 1");
+    this.itens.push("item 1");
+
+    for(let i = 0; i < this.itens.length;i++){
+      let item = this.itens[i];
+      console.log(item);
+
+    }
+    
+  }
   
 }
