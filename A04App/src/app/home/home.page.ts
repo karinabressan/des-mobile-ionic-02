0158a0debf3cefa03ea3d4e20c293383f86535a1
Karina <karina.felicetti@ngi.com.br>
2023-04-30 23:17:26 -0300

    Criando botão de alterar registro e salvar alteração. Criando modo de edição. Habilitando o botão salvar apenas no modo edição. Criando métodos de alteração.

--- A04App/src/app/home/home.page.ts
+++ A04App/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AlertController, IonicModule, ToastController } from '@ionic/angular';
 import { Produto } from '../produto/produto';
@@ -11,22 +11,50 @@ import { Produto } from '../produto/produto';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  novoProduto: string = "";
-  novaQuantidade: number = 1;
+  inputNomeProduto: string = "";
+  inputQuantidade: number = 1;
+  modoEdicao: boolean = false;
+  indiceEdicao: number = 0;
 
   message = "item adicionado";
   itens: Produto[] = [];
 
 
-  constructor(private alertController: AlertController, private toastController: ToastController) { }
+  constructor(private alertController: AlertController, private toastController: ToastController) { 
+  }
 
-  addToList(item: string, quantidade: number) {
-    if (item === undefined || item.trim() === "") {
+  ngOnInit(): void {
+ //   this.itens.push(new Produto('ss', 1));
+  }
+
+  startEditing(index:number){
+    let produtoEditando:Produto = this.itens[index];
+    this.inputNomeProduto = produtoEditando.nome;
+    this.inputQuantidade = produtoEditando.quantidade;
+    this.indiceEdicao = index;
+    this.modoEdicao = true;
+  }
+
+  update(){
+    if(this.modoEdicao){
+      let produtoEditando:Produto = this.itens[this.indiceEdicao];
+      produtoEditando.nome = this.inputNomeProduto;
+      produtoEditando.quantidade = this.inputQuantidade;
+      this.indiceEdicao = 0;
+      this.modoEdicao = false;
+    }
+
+  }
+
+  addToList() {
+    if (this.inputNomeProduto === undefined || this.inputNomeProduto.trim() === "") {
       this.showAlert("O campo de produto não poderá ser vazio");
+    } else if (this.inputQuantidade <= 0 ){
+      this.showAlert("Informe uma quantidade válida");
     } else {
-      this.itens.push(new Produto(item, quantidade));
+      this.itens.push(new Produto(this.inputNomeProduto, this.inputQuantidade));
       this.showToast("Produto adicionado");
     }
   }
